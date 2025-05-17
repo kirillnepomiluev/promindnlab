1eaf2e7c152d7678dc8baea2ddc371601da72683
Kirill Nepomiluev <47185819+kirillnepomiluev@users.noreply.github.com>
2025-05-17 08:56:44 +0300

    refactor(voice): inject ConfigService

--- src/voice/voice.module.ts
+++ src/voice/voice.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { VoiceService } from './voice.service/voice.service';
 
 @Module({
+  imports: [ConfigModule], // подключаем ConfigModule, если не был подключён
   providers: [VoiceService],
   exports: [VoiceService], // ← обязательно экспортируем!
 })
