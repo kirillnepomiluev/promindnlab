ab7b44e6f5bda01c64d39dfd90e975f9c1dcf5ea
StanleyFishman <117282489+StanleyFishman@users.noreply.github.com>
2025-07-16 15:15:13 +0500

    feat: process paid orders

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -8,6 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserProfile } from '../user/entities/user-profile.entity';
 import { UserTokens } from '../user/entities/user-tokens.entity';
 import { TokenTransaction } from '../user/entities/token-transaction.entity';
+import { OrderIncome } from '../user/entities/order-income.entity';
 import { MainUser } from '../external/entities/main-user.entity';
 import { MainOrder } from '../external/entities/order.entity';
 
@@ -22,7 +23,7 @@ import { MainOrder } from '../external/entities/order.entity';
       }),
     }),
     // Регистрируем репозитории для локальной и основной БД
-    TypeOrmModule.forFeature([UserProfile, UserTokens, TokenTransaction]),
+    TypeOrmModule.forFeature([UserProfile, UserTokens, TokenTransaction, OrderIncome]),
     TypeOrmModule.forFeature([MainUser, MainOrder], 'mainDb'),
     OpenaiModule,
     VoiceModule,
